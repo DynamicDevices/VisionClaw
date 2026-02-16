47b1a3418eb1c730cf4395c1e682b65d1790349c
Alex J Lennon <ajlenon@dynamicdevices.co.uk>
2026-02-16 16:15:10 +0000

    Android: add Compose Compiler plugin for Kotlin 2.0+ (required when compose enabled)

--- samples/CameraAccessAndroid/app/build.gradle.kts
+++ samples/CameraAccessAndroid/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -33,9 +34,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.10"
     }
 }
 
